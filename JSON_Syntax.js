891ef18f89cf63c43252fe8779283f484c984d36
Mukarram billah <103162305+MKBbillah@users.noreply.github.com>
2024-04-14 12:29:09 +0600

    Add files via upload

---
+++ JSON_Syntax.js
@@ -0,0 +1,18 @@
+var student = {
+    name: "Rahim",
+    age: 26,
+    hometown: "Dhaka"
+};
+
+var student_json = JSON.stringify(student);
+
+console.log(student_json);
+
+var student_new = JSON.parse(student_json);
+console.log(student_new);
+
+//var student_json = {
+  //  "name" : "Rahim" ,
+  //  "age" : 26,
+  //  "hometown" : "Khulna"
+//};
